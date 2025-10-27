4277b789b252bdeee3c6a0441b45744ef261081c
Loving you is like loving DJ <tonyya92@outlook.com>
2025-10-27 23:53:48 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,7 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
+          "自建【vless reality+hy1+hy2】": "https://subio.zeabur.app",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
@@ -347,7 +347,7 @@ export default {
           "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
-          { value: "https://subapi.cmliussss.net" },
+          { value: "https://subio.zeabur.app" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
           { value: "https://sub.d1.mk" },
@@ -1310,3 +1310,4 @@ export default {
   }
 };
 </script>
+
